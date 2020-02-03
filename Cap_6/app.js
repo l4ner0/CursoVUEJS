7e0ee1466d61d99d5f9f4122c8f342b6957e71a1
Diego <diegourbina1284@gmail.com>
2020-02-02 23:56:35 -0500

    Cap6

---
+++ Cap_6/app.js
@@ -0,0 +1,9 @@
+new Vue({
+    el: '#app',
+    data(){
+        return {
+            name: 'Bitcoin',
+            img: 'https://claveprivada.com/wp-content/uploads/2018/10/1024px-Bitcoin.svg.png'
+        }
+    }
+})
